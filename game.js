2c64a346a3f62f93d2b85d8366ab93c32392bc95
Majda-Fg <majda.fougou@gmail.com>
2024-10-31 10:38:00 +0100

    bouton suivant dynamique

--- game.js
+++ game.js
@@ -3,23 +3,30 @@ import { quiz_js } from './questions.js';
 const questions = document.querySelector('.question');
 const choice = document.querySelector(".options");
 
-// const expl = document.querySelector(".explication");
-
 let currentIndex = 0;
 
-const currentQuestion = quiz_js.questions[currentIndex];
-questions.innerText = currentQuestion.text;
+const next = document.getElementById('next-button')
 
-currentQuestion.options.forEach(option => {
+function loadQuestion(){
+  choice.innerHTML='';
+  const currentQuestion = quiz_js.questions[currentIndex];
+  questions.innerText = currentQuestion.text;
+  currentQuestion.options.forEach(option => {
     const bouton = document.createElement('button');
     bouton.innerText = option;
     bouton.classList.add('button');
     choice.appendChild(bouton);
   });  
+}
+next.addEventListener('click', () => {
+  currentIndex++;
+  if(currentIndex<quiz_js.questions.length){
+    loadQuestion();
+  }else{
+    next.innerText='The end';
+    choice.innerHTML='';
+    next.style.display='none';
+  }
+});
 
-// for(let i=0; i<questions.length; i++){
-//    console.log(questions[i].options);
-//}
-
-//   option.innerText += `<button>${quiz_js.questions[0].options}</button>`;
-
+loadQuestion();
